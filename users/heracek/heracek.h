aae339af795a75367c2cbf420b210d8d55a8f147
Tomáš Horáček <tomas.horacek@gmail.com>
2019-06-11 00:59:56 +0200

    Add heracek user and apply to helix keymap

---
+++ users/heracek/heracek.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "process_records.h"
+#include "custom_keycodes.h"
+#include "layouts.h"
+
+void my_custom_function(void);
---
+++ users/heracek/process_records.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "quantum.h"
+/* #include "custom_tap_dance.h" */
+
+enum layer_number {
+    _QWERTY = 0,
+    _LOWER,
+    _RAISE,
+    _ADJUST
+};
+
+/* bool process_record_keymap(uint16_t keycode, keyrecord_t *record); */
